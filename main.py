d766fbfa6e5e155d542a41399c66b7f01a6a6106
Dev Shah <devshah1032@gmail.com>
2021-09-22 08:16:30 +0530

    updates & db meme code completed

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ choice= int(input("Enter the number : "))
 
 if choice == 1:
     drake = Image.open("drake.jpg")
-    Result1 = Image.open('drake.jpg')
     draw=ImageDraw.Draw(drake)
     font = ImageFont.truetype("Font.ttf",27)
     Text1 = input("Enter drake text(first row) : ")
@@ -28,5 +27,22 @@ if choice == 1:
     draw.text((355,430),Text2,(0,0,0),font=font)
     Result = drake.save("Meme.png")
     print("Image saved as Meme.png")
+elif choice==2:
+    DisBoy = Image.open("distractedBoyfriend.jpg")
+    draw=ImageDraw.Draw(DisBoy)
+    font = ImageFont.truetype("Font.ttf",27)
+    font1 = ImageFont.truetype("Font.ttf",60)
+    font2 = ImageFont.truetype("Font.ttf",48)
+
+
+    Text1 = input("Enter Original GF text: ")
+    Text2 = input("Enter BF text: ")    
+    Text3 = input("Enter Other girl text : ")    
+    draw.text((700,210),Text1,(0,0,0),font=font)
+    draw.text((595,400),Text2,(0,0,0),font=font1)
+    draw.text((175,450),Text3,(0,0,0),font=font2)
+    Result = DisBoy.save("Meme1.png") 
+
+  
 else : 
     print("You messed up !")
